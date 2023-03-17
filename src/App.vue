c15c03a1f861aea863fe70d7cc3ee35aa70253c8
Astryda Malinowska <astamalinowska@outlook.com>
2023-03-17 23:46:56 +0100

    Finished app for injections

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,43 @@
       Lista zastrzyków użytkownika {{ name }}
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Zastrzyk</div>
-        <div class="col-2 font-weight-bold">Zakończono?</div>
+      <div class="row" v-if="filteredInjections.length == 0">
+        <div class="col text-center">
+          <b>Nie masz zastrzyków do zrobienia. Wspaniale!</b>
+        </div>
       </div>
-      <div class="row" v-for="i in filteredInjections" v-bind:key="i.action">
-        <div class="col">{{ i.action }}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="i.done" class="form-check-input" />
-          {{ i.done }}
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Zastrzyk</div>
+          <div class="col-2 font-weight-bold">Wykonano?</div>
+        </div>
+        <div class="row" v-for="i in filteredInjections" v-bind:key="i.action">
+          <div class="col">{{ i.action }}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="i.done" class="form-check-input" />
+            <!-- {{ i.done }} -->
+          </div>
+        </div>
+      </template>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newInjectionText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewInjection">Dodaj</button>
         </div>
       </div>
       <div class="row bg-secondary py-2 mt-2 text-white">
         <div class="col text-center">
           <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
           <label class="form-check-label font-weight-bold">
-            Ukryj zakończone zastrzyki
+            Ukryj wykonane zastrzyki
           </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Usuń wykonane
+          </button>
         </div>
       </div>
     </div>
@@ -33,19 +53,38 @@
     data() {
       return {
         name: "Astryda",
-        injections: [
-          { action: "Pierwszy zastrzyk - 14 marca", done: true },
-          { action: "Drugi zastrzyk - 20 marca", done: false },
-          { action: "Trzeci zastrzyk - 24 marca", done: false },
-          { action: "Czwarty zastrzyk - 30 marca", done: false }
-        ],
-        hideCompleted: true
+        injections: [],
+        hideCompleted: true,
+        newInjectionText: "",
       }
     },
     computed: {
       filteredInjections() {
         return this.hideCompleted ?
           this.injections.filter(i => !i.done) : this.injections
+      }
+    },
+    methods: {
+      addNewInjection() {
+        this.injections.push({
+          action: this.newInjectionText,
+          done: false
+        });
+        this.storeData();
+        this.newInjectionText = "";
+      },
+      storeData() {
+        localStorage.setItem("injections", JSON.stringify(this.injections));
+      },
+      deleteCompleted() {
+        this.injections = this.injections.filter(i => !i.done);
+        this.storeData();
+      }
+    },
+    created() {
+      let data = localStorage.getItem("injections");
+      if (data != null) {
+        this.injections = JSON.parse(data);
       }
     }
   }
