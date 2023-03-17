a8cab0c06583eee2bfad35976ce85ae078696854
Astryda Malinowska <astamalinowska@outlook.com>
2023-03-17 23:15:59 +0100

    Added hiding completed injections

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,22 @@
     <div class="container-fluid p-4">
       <div class="row">
         <div class="col font-weight-bold">Zastrzyk</div>
-        <div class="col-2 font-weight-bold">Zakończono</div>
+        <div class="col-2 font-weight-bold">Zakończono?</div>
       </div>
-      <div class="row" v-for="i in injections" v-bind:key="i.action">
+      <div class="row" v-for="i in filteredInjections" v-bind:key="i.action">
         <div class="col">{{ i.action }}</div>
-        <div class="col-2">{{ i.done }}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="i.done" class="form-check-input" />
+          {{ i.done }}
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Ukryj zakończone zastrzyki
+          </label>
+        </div>
       </div>
     </div>
   </div>
@@ -26,8 +37,15 @@
           { action: "Pierwszy zastrzyk - 14 marca", done: true },
           { action: "Drugi zastrzyk - 20 marca", done: false },
           { action: "Trzeci zastrzyk - 24 marca", done: false },
-          { acrion: "Czwarty zastrzyk - 30 marca", done: false }
-        ]
+          { action: "Czwarty zastrzyk - 30 marca", done: false }
+        ],
+        hideCompleted: true
+      }
+    },
+    computed: {
+      filteredInjections() {
+        return this.hideCompleted ?
+          this.injections.filter(i => !i.done) : this.injections
       }
     }
   }
